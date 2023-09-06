ce41706a90fd19db26a7285318231ffd05b61c10
yjie10 <yy.jie28@gmail.com>
2023-09-05 17:12:30 -0700

    var types

--- basic.ts
+++ basic.ts
@@ -1,5 +1,12 @@
+// we use the syntax "variableName: type" to annotate variables
+
 let pi:number = 3.14159;
 
 let movieTitle: string = "Fave Movie";
 movieTitle = "Another Fave Movie";
 // movieTitle = 9; // can't reassign it to a different type
+
+let num: number = 28;
+num = 318; // CAN reassign to a value of same type
+
+const myBool: boolean = true; // type Booleans
